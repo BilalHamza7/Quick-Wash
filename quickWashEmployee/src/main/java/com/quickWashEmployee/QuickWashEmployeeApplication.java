11f56e24e8d4fbd3395621e12c4bdb9b9e673d17
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2023-10-08 15:56:02 +0530

    Add files via upload

---
+++ quickWashEmployee/src/main/java/com/quickWashEmployee/QuickWashEmployeeApplication.java
@@ -0,0 +1,14 @@
+package com.quickWashEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class QuickWashEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickWashEmployeeApplication.class, args);
+	}
+		//runs on port 8081
+}
---
+++ quickWashEmployee/src/main/java/com/quickWashEmployee/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.quickWashEmployee.service;
+
+import com.quickWashEmployee.data.Employee;
+import com.quickWashEmployee.data.EmployeeRepository;
+import com.quickWashEmployee.exception.EmployeeNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        List<Employee> employee = employeeRepository.findAll();
+        return employee;
+    }
+
+    public Employee getEmployeeById(int id) {
+        Optional<Employee> emp = this.employeeRepository.findById(id);
+        return emp.get();
+    }
+
+    public Employee updateEmployee(Employee employee, int id) {
+        return employeeRepository.findById(id)
+                .map(employee1 -> {
+                    employee1.setName(employee.getName());
+                    employee1.setAddress(employee.getAddress());
+                    employee1.setMobileNumber(employee.getMobileNumber());
+                    return employeeRepository.save(employee1);
+                }).orElseThrow(()-> new EmployeeNotFoundException(id));
+    }
+
+    public void deleteEmployee(int id) {
+
+        employeeRepository.deleteById(id);
+    }
+}
